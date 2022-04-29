f0af27d47638a20ca82f6720a67948aa0509d758
ali1sba <hariri.ali.official@gmail.com>
2022-04-29 17:10:33 +0100

    adding notFound and home views

---
+++ src/views/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <!-- TODO: create the not found -->
+  <!-- deviding screen into two columns: sidebar and content -->
+  <h1>Page Not found</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
