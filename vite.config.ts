1a072361e38942fa489ecade7cc34e31ef4434e3
nour <grebici.nour@gmail.com>
2022-06-16 09:38:50 +0100

    CONFIG

--- vite.config.ts
+++ vite.config.ts
@@ -8,66 +8,66 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 export default defineConfig({
   server: {
     host: "127.0.0.1",
-    port: 8090,
+    port: 8091,
     // port: 3000,
     proxy: {
       "/api/payment/purchase": {
-        target: "http://127.0.0.1:5003",
+        target: "http://192.168.14.71:7777/service-purchase/",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/home/, ""),
       },
       "/api/admin/codepromo/check": {
-        target: "http://127.0.0.1:5003",
+        target: "http://192.168.14.71:7777/service-purchase/",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/home/, ""),
       },
       "/api/common/forgetPasswordSendVerificationAccount": {
-        target: "http://127.0.0.1:5001",
+        target: "http://192.168.14.71:7777/service-auth/",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/home/, ""),
       },
       "/api/client/profile": {
-        target: "http://127.0.0.1:5001",
+        target: "http://192.168.14.71:7777/service-auth/",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/home/, ""),
       },
       "/api/client/verifyCode": {
-        target: "http://127.0.0.1:5001",
+        target: "http://192.168.14.71:7777/service-auth/",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/home/, ""),
       },
       "/api/admin/event**": {
-        target: "http://127.0.0.1:5002",
+        target: "http://192.168.14.71:7777/service-event/",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/home/, ""),
       },
       "/home/api": {
-        target: "http://127.0.0.1:5002",
+        target: "http://192.168.14.71:7777/service-event/",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/home/, ""),
       },
       "/home/subList/api": {
-        target: "http://127.0.0.1:5002",
+        target: "http://192.168.14.71:7777/service-event/",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/home\/subList/, ""),
       },
 
       "/api/admin/event": {
-        target: "http://127.0.0.1:5002",
+        target: "http://192.168.14.71:7777/service-event/",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/home/, ""),
       },
       "/api": {
-        target: "http://127.0.0.1:5001",
+        target: "http://192.168.14.71:7777/service-auth/",
         changeOrigin: true,
         secure: false,
       },
